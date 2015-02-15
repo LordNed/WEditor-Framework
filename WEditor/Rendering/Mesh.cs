a8a3157b8b49c275cf3fe563058a03a2f8c51828
Matt Hoffman <matt@luxeloperation.com>
2015-02-15 14:03:58 -0800

    Added a stub for the Mesh class.

--- WEditor/Rendering/Mesh.cs
+++ WEditor/Rendering/Mesh.cs
@@ -1,7 +1,38 @@
-﻿namespace WEditor.Rendering
+﻿using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace WEditor.Rendering
 {
-    public 
-    class Mesh
+    public class Mesh
     {
+        /// <summary> The bounding volume of the mesh. This is an axis-aligned bounding box in local space. Setting Verticies will automatically update the Bounds. (Read Only) </summary>
+        public Bounds Bounds { get; private set; }
+        /// <summary> Vertex colors of the mesh. </summary>
+        public Color[] Colors;
+        /// <summary> Vertex normals of the mesh. </summary>
+        public Vector3[] Normals;
+        /// <summary> Triangle indexes of the mesh. </summary>
+        public int[] Triangles;
+        /// <summary> Vertex positions of the mesh. </summary>
+        public Vector3[] Verticies;
+        /// <summary> Texture Coordinates of the mesh. </summary>
+        public Vector2[] UVs;
+        /// <summary> Vertex count of the mesh. (Read Only) </summary>
+        public int VertexCount { get; private set; }
+        /// <summary> MeshTopology lets you render complex things using lines or points instead of just triangles if desired. </summary>
+        //public PrimitiveType MeshTopology;
+
+        public Mesh()
+        {
+            Bounds = new Bounds(Vector3.Zero, Vector3.Zero);
+            Colors = null;
+            Normals = null;
+            Triangles = null;
+            Verticies = null;
+            UVs = null;
+            VertexCount = 0;
+            //MeshTopology = PrimitiveType.Triangles;
+        }
     }
 }
